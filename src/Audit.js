bd70da259c63a5d8d609620aede90e96b6175b6d
Liran Tal <liran.tal@gmail.com>
2019-12-13 23:50:52 -0500

    feat(auth): authenticate and then run tests

--- src/Audit.js
+++ src/Audit.js
@@ -6,9 +6,13 @@ const ChildProcess = require('child_process')
 const ExecFile = Util.promisify(ChildProcess.execFile)
 
 const auditCliCommand = 'node_modules/snyk/dist/cli/index.js'
+const ERROR_VULNS_FOUND = 1
+const ERROR_UNAUTHENTICATED = 2
 
 class Audit {
-  async authenticate() {}
+  async authenticate() {
+    return ExecFile(auditCliCommand, ['auth'])
+  }
 
   async test() {
     let testResults = []
@@ -16,8 +20,17 @@ class Audit {
     try {
       await ExecFile(auditCliCommand, ['test', '--json'])
     } catch (error) {
-      if (error.code === 1) {
+      if (error.code === ERROR_VULNS_FOUND) {
+        // we are authenticated as a user for Snyk
+        // but vulnerabilities have been found
         testResults = JSON.parse(error.stdout)
+      } else if (error.code === ERROR_UNAUTHENTICATED) {
+        // user is not authenticated and and we need
+        // to trigger the auth process
+        await this.authenticate()
+        return this.test()
+      } else {
+        throw error
       }
     }
 
