73c79ba1def3d18d6496018c344fe7333b75b8e5
Liran Tal <liran.tal@gmail.com>
2019-12-14 00:42:24 -0500

    refactor(auth): use stream for on-the-fly process messages

--- src/Audit.js
+++ src/Audit.js
@@ -11,7 +11,26 @@ const ERROR_UNAUTHENTICATED = 2
 
 class Audit {
   async authenticate() {
-    return ExecFile(auditCliCommand, ['auth'])
+    return new Promise((resolve, reject) => {
+      const process = ChildProcess.execFile(auditCliCommand, ['auth'])
+      process.stdout.on('data', chunk => {
+        const httpsLinkMatch = chunk.match(/https:\/\/.*/g)
+        if (httpsLinkMatch && httpsLinkMatch.length > 0) {
+          const httpsLink = httpsLinkMatch[0].trim()
+          console.log()
+          console.log(`or you can hit this link:`)
+          console.log(`  ${httpsLink}`)
+        }
+      })
+
+      process.on('close', exitCode => {
+        return resolve()
+      })
+
+      process.on('error', error => {
+        return reject(new Error(error))
+      })
+    })
   }
 
   async test() {
@@ -25,8 +44,11 @@ class Audit {
         // but vulnerabilities have been found
         testResults = JSON.parse(error.stdout)
       } else if (error.code === ERROR_UNAUTHENTICATED) {
-        // user is not authenticated and and we need
+        // user is not authenticated and we need
         // to trigger the auth process
+
+        console.log(`Seems like you're not authenticated to Snyk,`)
+        console.log(`so redirecting you now and after login I'll show scan results here`)
         await this.authenticate()
         return this.test()
       } else {
