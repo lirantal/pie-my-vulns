35f49d43aa5375d34a87fbffb2edbf54e03c3f81
dkorn <korndaniel1@gmail.com>
2020-03-26 21:10:42 +0200

    fix: re-design summary output

--- __tests__/__e2e__/cli.e2e.js
+++ __tests__/__e2e__/cli.e2e.js
@@ -39,8 +39,8 @@ describe('End-to-End CLI', () => {
       expect(err.stdout).toContain('Patchable vulnerabilities (0.00%)')
       expect(err.stdout).toContain('No remediation available (0.00%)')
       expect(err.stdout).toContain('Scan completed successfully')
-      expect(err.stdout).toContain('Total number of vulnerabilities found:')
-      expect(err.stdout).toContain('Number of dependencies scanned:')
+      expect(err.stdout).toContain('Total number of vulnerabilities found')
+      expect(err.stdout).toContain('Total number of dependencies scanned')
       expect(err.stdout).toContain('Vulnerabilities by severity:')
       expect(err.stdout).toContain('Vulnerabilities by remediation action:')
     }
